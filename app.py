4862ff4482c5944f732cdebd69d5b7b62c42fddc
Christopher Final <131488208+cfinal15@users.noreply.github.com>
2025-05-15 22:28:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.schema import Document
 import json
 import os
